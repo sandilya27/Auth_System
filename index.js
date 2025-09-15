048b32d85f9baf199a7cd77ed4b89d72f139197d
sandilya27 <sandilyasanatan7@gmail.com>
2025-09-15 17:59:07 +0530

    feat: created utility for sending mail

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+
 import db from "./utils/db.js";
+
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors({
+app.use(
+  cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-}));
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,6 +26,8 @@ app.get("/", (_req, res) => {
 });
 
 db();
+
+app.use("/api/v1/users", userRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ utils/email.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const sendEmail = async ({ to, subject, html }) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAILTRAP_HOST,
+      port: Number(process.env.MAILTRAP_PORT),
+      secure: false,
+      auth: {
+        user: process.env.MAILTRAP_USERNAME,
+        pass: process.env.MAILTRAP_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.MAILTRAP_SENDEREMAIL,
+      to,
+      subject,
+      html,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: %s", info.messageId);
+    return info;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+};
